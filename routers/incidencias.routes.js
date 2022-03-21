c3f4b581b6a597f683fe4ef567316b9a3120eb8e
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-03-21 06:20:46 +0100

    Incidencias routes

---
+++ routers/incidencias.routes.js
@@ -0,0 +1,12 @@
+const {Router} = require("express");
+const incidenciaCtrl = require("../controllers/incidencias.controller");
+
+
+const router = Router();
+
+router.get("/incidencias",incidenciaCtrl.getIncidencias);
+router.post("/incidencia",incidenciaCtrl.postIncidencia);
+router.put("/incidencia",incidenciaCtrl.putIncidencia);
+router.delete("/incidencia",incidenciaCtrl.delIncidencia);
+
+module.exports = router;
