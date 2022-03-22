8fb1f016f2f135ef256eac59458ff9168e837535
Zetsuga <darkbear24@gmail.com>
2022-03-22 19:10:37 +0100

    actualizacion

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -47,7 +47,7 @@ function getUsuario(request,response){
 function postUsuario(request,response){
     let {nombre,apellidos,telefono,direccion,cp,poblacion,ciudad,rol,num_cuenta,email,contrasenia} = request.body;
     param = [nombre,apellidos,telefono,direccion,cp,poblacion,ciudad,contrasenia,rol,num_cuenta,email];
-    sql = "INSERT INTO usuario(nombre,apellidos,telefono,direccion,cp,poblacion,ciudad,contrasenia,rol,num_cuenta,email) VALUE ?";
+    sql = "INSERT INTO usuario (nombre,apellidos,telefono,direccion,cp,poblacion,ciudad,contrasenia,rol,num_cuenta,email) VALUE (?)";
 console.log(sql,param)
     connection.query(sql,param,function(err,result){
         if(err){
