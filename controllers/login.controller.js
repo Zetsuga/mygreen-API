b6310680ff8fc9ae3f0ead102ebf3736c000fd26
Zetsuga <darkbear24@gmail.com>
2022-03-27 19:16:21 +0200

    actualizacion endpoint usuario

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,20 +1,21 @@
 const { response } = require("express");
 const connection = require("../database");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt')
 
 let sql;
 let param;
 let respuesta ={};
 
-function postLogin(request,response){
-    param = [request.body.email,request.body.contrasenia];
+async function postLogin(request,response){
+
+    let contrasenia = await bcrypt.hash(request.body.contrasenia,10)
+    param = [request.body.email,contrasenia];
     sql = "SELECT usu.*,finc.id_finca FROM usuario AS usu "+
         "JOIN usuarios_fincas AS usuf ON (usu.id_usuario = usuf.id_usuario) " +
         "JOIN finca AS finc ON (finc.id_finca = usuf.id_finca) "+
         "WHERE email = ? AND contrasenia = ?";
 
     connection.query(sql,param,function(err,result){
-        console.log(result[0].contrasenia);
         if(err){
             console.log(err)
             respuesta = {
@@ -25,8 +26,7 @@ function postLogin(request,response){
                 resultado : "-1"
             }   
         }else{
-            let compare = bcrypt.compareSync(request.body.contrasenia,result.contrasenia)
-            if(result.length>0 && compare){
+            if(result.length>0){
                 respuesta = {
                     error: false,
                     codigo: 200,
@@ -36,18 +36,17 @@ function postLogin(request,response){
                 } 
             }else{
                 respuesta = {
-                    error: false,
+                    error: true,
                     codigo: 200,
-                    mensaje: `Contraseña no válida`,
-                    titulo:"Login incorrecto",
-                    resultado : -1
-                }
+                    mensaje: `Login incorrecto`,
+                    titulo:"error en el login",
+                    resultado : result
+                } 
             }
              
         }
         response.send(respuesta);
     })
 }
-
 
 module.exports = {postLogin}
