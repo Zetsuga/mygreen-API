63da636b47c6cb3dfe1f018a42a8deccbc4a3288
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-03-27 01:21:13 +0100

    Cambios nominas

--- routers/nominas.routes.js
+++ routers/nominas.routes.js
@@ -5,7 +5,6 @@ const router = Router();
 
 router.get("/nominas", nominasCtrl.getNomina);
 router.post("/nominas", nominasCtrl.postNomina);
-//router.post("/nominas", nominasCtrl.postNominaFichero);
 router.delete("/nominas", nominasCtrl.delNomina);
 
 module.exports = router;
