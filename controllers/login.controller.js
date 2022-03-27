6d2a73e6db82e5466dc3227ecc8565c323fc8c38
Zetsuga <darkbear24@gmail.com>
2022-03-27 19:23:51 +0200

    actualizacion endpoint usuario

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -8,13 +8,11 @@ let respuesta ={};
 
 async function postLogin(request,response){
 
-    let contrasenia = await bcrypt.hash(request.body.contrasenia,10)
-    console.log(contrasenia);
-    param = [request.body.email,contrasenia];
+    param = [request.body.email];
     sql = "SELECT usu.*,finc.id_finca FROM usuario AS usu "+
         "JOIN usuarios_fincas AS usuf ON (usu.id_usuario = usuf.id_usuario) " +
         "JOIN finca AS finc ON (finc.id_finca = usuf.id_finca) "+
-        "WHERE email = ? AND contrasenia = ?";
+        "WHERE email = ?";
 
     connection.query(sql,param,function(err,result){
         if(err){
@@ -27,7 +25,8 @@ async function postLogin(request,response){
                 resultado : "-1"
             }   
         }else{
-            if(result.length>0){
+            let compare = bcrypt.compareSync(request.body.contrasenia,result[0].contrasenia)
+            if(result.length>0 && compare){
                 respuesta = {
                     error: false,
                     codigo: 200,
