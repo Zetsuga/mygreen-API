60975e1bfb2b0969388f8e8c9ab09b1622ea5106
Zetsuga <darkbear24@gmail.com>
2022-03-29 20:08:21 +0200

    endpoint recordar contraseña

--- controllers/recordarPassword.controller.js
+++ controllers/recordarPassword.controller.js
@@ -1,19 +1,18 @@
 const nodemailer = require('nodemailer');
-const { response } = require("express");
 const connection = require("../database");
+const bcrypt = require("bcrypt");
 
 let sql;
 let param;
 let respuesta ={};
 
 
-function recordarContrasenia(request,response){
+async function recordarContrasenia(request,response){
     param = [request.body.email];
     sql = "SELECT * FROM usuario WHERE email = ?";
 
 
     connection.query(sql,param,function(err,result){
-        console.log(result);
         if(err){
             respuesta = {
                 error: true,
@@ -23,40 +22,59 @@ function recordarContrasenia(request,response){
                 resultado : "-1"
             }   
         }else{
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'jjp.desarrolladores@gmail.com', // Cambialo por tu email
-                    pass: 'jjp2022@.' // Cambialo por tu password
-                }
-            });
-        
-            const mailOptions = {
-                from: `”Mygreen” <jjp.desarrolladores@gmil.com>`,
-                to: result[0].email, // Cambia esta parte por el destinatario
-                subject: "Credenciales de usuario",
-                html: `
-                    <strong>Nombre:</strong> ${result[0].nombre} <br/>
-                    <strong>E-mail:</strong> ${result[0].email} <br/>
-                    <strong>Contrasenia:</strong> ${result[0].contrasenia}
-                `
-            };
+            let contrasenia = await bcrypt.hash("123456");
+            param = [contrasenia,result[0].id_usuario];
+            sql = "UPDATE usuario SET contrasenia = COALESCE(?,contrasenia) WHERE id_usuario = ?";
             
-            transporter.sendMail(mailOptions, function (err, info) {
-                if (err)
-                    console.log(err)
-                else
+            connection.query(sql,param,function(err,result){
+                if(err){
                     respuesta = {
-                        error: false,
+                        error: true,
                         codigo: 200,
-                        mensaje: "Contraseña enviada",
-                        titulo:"Revisa tu correo para la nueva contraseña",
-                        resultado : "1"
+                        mensaje: "Usuarios no encontrados",
+                        titulo:"Error al buscar",
+                        resultado : "-1"
                     }   
+                }else{
+                    var transporter = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                            user: 'jjp.desarrolladores@gmail.com', // Cambialo por tu email
+                            pass: 'jjp2022@.' // Cambialo por tu password
+                        }
+                    });
+                
+                    const mailOptions = {
+                        from: `”Mygreen” <jjp.desarrolladores@gmil.com>`,
+                        to: result[0].email, // Cambia esta parte por el destinatario
+                        subject: "Petición de datos",
+                        html: `
+                            <strong>Nombre:</strong> ${result[0].nombre} <br/>
+                            <strong>E-mail:</strong> ${result[0].email} <br/>
+                            <strong>Contrasenia:</strong> 123456
+                        `
+                    };
+                    
+                    transporter.sendMail(mailOptions, function (err, info) {
+                        if (err)
+                            console.log(err)
+                        else
+                            respuesta = {
+                                error: false,
+                                codigo: 200,
+                                mensaje: "Contraseña enviada",
+                                titulo:"Revisa tu correo para la nueva contraseña",
+                                resultado : "1"
+                            }   
+                    });
+            
+                    response.send(respuesta);
+                }
+                
             });
         }
-        response.send(respuesta)
-    })
+    
+    });
 }
 
 module.exports = {recordarContrasenia}
