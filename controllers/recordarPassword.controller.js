101da6715eb7213b79320390b97dd6fd08a79bf1
Zetsuga <darkbear24@gmail.com>
2022-03-29 19:40:06 +0200

    endpoint recordar contraseña

--- controllers/recordarPassword.controller.js
+++ controllers/recordarPassword.controller.js
@@ -14,48 +14,48 @@ function recordarContrasenia(request,response){
 
     connection.query(sql,param,function(err,result){
         console.log(result);
-        // if(err){
-        //     respuesta = {
-        //         error: true,
-        //         codigo: 200,
-        //         mensaje: "Usuarios no encontrados",
-        //         titulo:"Error al buscar",
-        //         resultado : "-1"
-        //     }   
-        // }else{
-        //     var transporter = nodemailer.createTransport({
-        //         service: 'gmail',
-        //         auth: {
-        //             user: 'jjp.desarrolladores@gmail.com', // Cambialo por tu email
-        //             pass: 'jjp2022@.' // Cambialo por tu password
-        //         }
-        //     });
+        if(err){
+            respuesta = {
+                error: true,
+                codigo: 200,
+                mensaje: "Usuarios no encontrados",
+                titulo:"Error al buscar",
+                resultado : "-1"
+            }   
+        }else{
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: 'jjp.desarrolladores@gmail.com', // Cambialo por tu email
+                    pass: 'jjp2022@.' // Cambialo por tu password
+                }
+            });
         
-        //     const mailOptions = {
-        //         from: `”Mygreen” <jjp.desarrolladores@gmil.com>`,
-        //         to: result[0].email, // Cambia esta parte por el destinatario
-        //         subject: "Credenciales de usuario",
-        //         html: `
-        //             <strong>Nombre:</strong> ${result[0].nombre} <br/>
-        //             <strong>E-mail:</strong> ${result[0].email} <br/>
-        //             <strong>Contrasenia:</strong> ${result[0].contrasenia}
-        //         `
-        //     };
+            const mailOptions = {
+                from: `”Mygreen” <jjp.desarrolladores@gmil.com>`,
+                to: result[0].email, // Cambia esta parte por el destinatario
+                subject: "Credenciales de usuario",
+                html: `
+                    <strong>Nombre:</strong> ${result[0].nombre} <br/>
+                    <strong>E-mail:</strong> ${result[0].email} <br/>
+                    <strong>Contrasenia:</strong> ${result[0].contrasenia}
+                `
+            };
             
-        //     transporter.sendMail(mailOptions, function (err, info) {
-        //         if (err)
-        //             console.log(err)
-        //         else
-        //             respuesta = {
-        //                 error: false,
-        //                 codigo: 200,
-        //                 mensaje: "Contraseña enviada",
-        //                 titulo:"Revisa tu correo para la nueva contraseña",
-        //                 resultado : "1"
-        //             }   
-        //     });
-        // }
-        // response.send(respuesta)
+            transporter.sendMail(mailOptions, function (err, info) {
+                if (err)
+                    console.log(err)
+                else
+                    respuesta = {
+                        error: false,
+                        codigo: 200,
+                        mensaje: "Contraseña enviada",
+                        titulo:"Revisa tu correo para la nueva contraseña",
+                        resultado : "1"
+                    }   
+            });
+        }
+        response.send(respuesta)
     })
 }
 
